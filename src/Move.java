51e90f479f32ba266e281b1043104c168b17ca03
NICHOLAS-1217 <nicholastan1217@gmail.com>
2023-12-20 04:49:56 +0800

    action listener not working

---
+++ src/Move.java
@@ -0,0 +1,18 @@
+public class Move {
+    int oldColumn;
+    int oldRow;
+    int newColumn;
+    int newRow;
+
+    Piece piece;
+    Piece capture;
+
+    public Move(Board board, Piece piece, int newColumn, int newRow) {
+        this.oldColumn = piece.column;
+        this.oldRow = piece.row;
+        this.newColumn = newColumn;
+        this.newRow = newRow;
+        this.piece = piece;
+        this.capture = board.getPiece(newColumn, newRow);
+    }
+}
